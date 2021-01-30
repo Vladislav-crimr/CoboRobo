aa9fc657c23bd5e77ee68ced45726fabbb4ef378
Vladislav-crir <bychkov09-ne@yandex.ru>
2021-01-30 15:31:11 +0300

    доработка игры

--- game_01/gamevlad.py
+++ game_01/gamevlad.py
@@ -1,10 +1,12 @@
 import random
+
+mySelf = True
 def fride():
-    fride = int(input(" Вы хотите открыть холодильник 1-да 2-нет "))
-    if fride == 1:
-        mom()
-    else:
-        print(" конец игры ")     
+    global mySelf
+    if mySelf == True:
+        fride = int(input(" Вы хотите открыть холодильник 1-да 2-нет "))
+        if fride == 1:
+            mom()
 
 
 def mom():
@@ -14,21 +16,35 @@ def mom():
     else:
         alarm()
 
-
 def alarm():
+    global mySelf
     alarm = int(input(" сработала сигнализацыя ты можеш спрятотся или остаться 1-остаться 2-спрятотся "))
     if alarm == 1:
         print(" пришла мать выпорела тебя и ты пошол спать голодный ")
+        mySelf = False
     else:
         dad()
 
 
 def dad():
+    global mySelf
     randDad = random.randint(1,1000)
     if randDad <= 500:
         print(" ты попытался спрятотся в туалет но там сидит батя")
+        mySelf = False
     else:
         fride()
 
 
-fride()
+def end():
+    global mySelf
+    end = int(input(" Ты хочеш сыграть ещё раз 1-да , 2-нет "))
+    if end == 1:
+        mySelf = True
+    else:
+        mySelf = False
+
+while mySelf:
+    fride()
+    end()
+
